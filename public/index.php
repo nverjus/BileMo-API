2ceffd59a0d1e74f45c1c869da0c078d8a5151dc
Nicolas Verjus <nverjus@protonmail.com>
2018-06-28 14:10:36 +0200

    Varnish proxy container setup

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Kernel;
-use App\CacheKernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,9 +33,6 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 }
 
 $kernel = new Kernel($env, $debug);
-if ('prod' === $env) {
-    $kernel = new CacheKernel($kernel);
-}
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
